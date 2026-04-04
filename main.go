8d2440798379c2faff5e5e8e021dcb368527ac1c
Aakash <aakash04unique@gmail.com>
2026-04-04 15:55:00 +0530

    Added Health Status API

--- main.go
+++ main.go
@@ -5,12 +5,23 @@ import (
 	"net/http"
 )
 
+func handlerHealth(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	_, err := w.Write([]byte(http.StatusText(http.StatusOK)))
+	if err != nil {
+		log.Printf("Error Writing the Body of the Message in the Health Handler: %v", err)
+	}
+
+}
+
 func main() {
 	const filePathRoot = "."
 	const port = "8080"
 
 	router := http.NewServeMux()
-	router.Handle("/", http.FileServer(http.Dir(filePathRoot)))
+	router.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filePathRoot))))
+	router.HandleFunc("/healthz", handlerHealth)
 
 	server := http.Server{
 		Handler: router,
