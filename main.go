1a959e1f0687e9ef6227d22ecfaf4e98eb3bc8f8
Aakash <aakash04unique@gmail.com>
2026-04-04 17:12:12 +0530

    Added Metrics counter

--- main.go
+++ main.go
@@ -1,9 +1,33 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (a *apiConfig) middlewareIncrementMetrics(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		a.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (a *apiConfig) logHits(w http.ResponseWriter, r *http.Request) {
+	_, err := w.Write([]byte(fmt.Sprintf("Hits: %v", a.fileserverHits.Load())))
+	if err != nil {
+		log.Printf("Error Writing the Body of the Message in the Metrics Handler: %v", err)
+	}
+}
+
+func (a *apiConfig) reset(w http.ResponseWriter, r *http.Request) {
+	a.fileserverHits.Store(0)
+}
 
 func handlerHealth(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
@@ -26,9 +50,36 @@ func main() {
 	const port = "8080"
 
 	fileHandler := http.StripPrefix("/app", http.FileServer(http.Dir(filePathRoot)))
+
+	config := apiConfig{
+		fileserverHits: atomic.Int32{},
+	}
+
 	router := http.NewServeMux()
-	router.Handle("/app/", middlewareLog(fileHandler))
-	router.Handle("/healthz", middlewareLog(http.HandlerFunc(handlerHealth)))
+	router.Handle("/app/",
+		middlewareLog(
+			config.middlewareIncrementMetrics(
+				fileHandler,
+			),
+		),
+	)
+	router.Handle("/healthz",
+		middlewareLog(
+			config.middlewareIncrementMetrics(
+				http.HandlerFunc(handlerHealth),
+			),
+		),
+	)
+	router.Handle("/metrics",
+		middlewareLog(
+			http.HandlerFunc(config.logHits),
+		),
+	)
+	router.Handle("/reset",
+		middlewareLog(
+			http.HandlerFunc(config.reset),
+		),
+	)
 
 	server := http.Server{
 		Handler: router,
