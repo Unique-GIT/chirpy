e9601f86cbcbb1c83f00cf3a4a2c5cd3c3923a68
Aakash <aakash04unique@gmail.com>
2026-04-04 16:37:18 +0530

    Logging every request

--- main.go
+++ main.go
@@ -12,16 +12,23 @@ func handlerHealth(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Printf("Error Writing the Body of the Message in the Health Handler: %v", err)
 	}
+}
 
+func middlewareLog(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("Served Request %s on %s", r.Method, r.URL.Path)
+		next.ServeHTTP(w, r)
+	})
 }
 
 func main() {
 	const filePathRoot = "."
 	const port = "8080"
 
+	fileHandler := http.StripPrefix("/app", http.FileServer(http.Dir(filePathRoot)))
 	router := http.NewServeMux()
-	router.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filePathRoot))))
-	router.HandleFunc("/healthz", handlerHealth)
+	router.Handle("/app/", middlewareLog(fileHandler))
+	router.Handle("/healthz", middlewareLog(http.HandlerFunc(handlerHealth)))
 
 	server := http.Server{
 		Handler: router,
